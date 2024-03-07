90afc95028f97e564bcbdb00d3c0905c02f1e66a
ValerioL29 <cheng2029@foxmail.com>
2024-03-07 02:24:06 +0000

    Update dependencies and project info

--- test.py
+++ test.py
@@ -8,28 +8,29 @@ prompt = "What are the things I should be cautious about when I visit here?"
 image_file = "https://llava-vl.github.io/static/images/view.jpg"
 
 def init_model():
-    pass
-    # tokenizer, model, image_processor, context_len = load_pretrained_model(
-    #     model_path=model_path,
-    #     model_base=None,
-    #     model_name=get_model_name_from_path(model_path)
-    # )
+    tokenizer, model, image_processor, context_len = load_pretrained_model(
+        model_path=model_path,
+        model_base=None,
+        model_name=get_model_name_from_path(model_path)
+    )
+    
 
-args = type('Args', (), {
-    "model_path": model_path,
-    "model_base": None,
-    "model_name": get_model_name_from_path(model_path),
-    "query": prompt,
-    "conv_mode": None,
-    "image_file": image_file,
-    "sep": ",",
-    "temperature": 0,
-    "top_p": None,
-    "num_beams": 1,
-    "max_new_tokens": 512,
-    "use_flash_attn": False, # Quadro RTX 6000 doesn't support FA2 as it's Turing arch 
-    "load_4bit": False,
-    "load_8bit": True,
-})()
+def eval_model():
+    args = type('Args', (), {
+        "model_path": model_path,
+        "model_base": None,
+        "model_name": get_model_name_from_path(model_path),
+        "query": prompt,
+        "conv_mode": None,
+        "image_file": image_file,
+        "sep": ",",
+        "temperature": 0,
+        "top_p": None,
+        "num_beams": 1,
+        "max_new_tokens": 512,
+        "use_flash_attn": False, # Quadro RTX 6000 doesn't support FA2 as it's Turing arch 
+        "load_4bit": False,
+        "load_8bit": True,
+    })()
 
-_ = eval_model(args)
+    _ = eval_model(args)
